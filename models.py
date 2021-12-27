f9b29f4b01adb579c7d75e6c371f0828e18cbdba
parisa <57132194+parisafarivashh@users.noreply.github.com>
2021-12-27 12:48:54 +0330

    Create member, message, room models (#6)
    * Add file models.py
    
    * create member & room models
    
    * create message model
    
    * add primary_key to table members_room
    
    * add comment connection
    
    * change style coding
    
    * change style code table member_room
    
    * add file requirements.txt
    
    * change name of model members_room to member_room
    
    * Add a secondary in relation to the member model
    
    * delete comment connection because state the obvious
    
    * delete table member_room
    
    * Delete the empty space next to the table
    
    * add extra line to the end of the project
    
    * change name members relation to member in room model
    
    * change name member relation to creator in room model
    
    * change name member relation to sender in message model
    
    * add space after table name and after imports
    
    * change name relation between member and room models
    
    * change name relation between member and message models
    
    * change name back_populate relation room in message model
    
    * change double quotation to single quotation

---
+++ models.py
@@ -0,0 +1,116 @@
+from sqlalchemy import Column, ForeignKey, \
+    Integer, String, create_engine, Date
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine(
+    'postgresql+psycopg2://postgres:postgres@localhost/sqlalchemy_practice'
+)
+Base = declarative_base()
+
+
+class Member(Base):
+    __tablename__ = 'member'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    title = Column(
+        'title',
+        String,
+        nullable=True,
+    )
+    first_name = Column(
+        'first_name',
+        String,
+        nullable=False,
+    )
+    last_name = Column(
+        'last_name',
+        String,
+        nullable=True,
+    )
+    birthday = Column(
+        'birthday',
+        Date,
+        nullable=True,
+    )
+    creator_room = relationship(
+        'Room',
+        back_populates='creator',
+        lazy='joined',
+    )
+    messages = relationship(
+        'Message',
+        back_populates='sender',
+        lazy='joined',
+    )
+
+
+class Room(Base):
+    __tablename__ = 'room'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    title = Column(
+        'title',
+        String,
+        nullable=False,
+    )
+    creator_id = Column(
+        'creator_id',
+        Integer,
+        ForeignKey('member.id'),
+    )
+    creator = relationship(
+        'Member',
+        order_by='room.id',
+        back_populates='creator_room',
+        lazy='joined',
+    )
+    messages = relationship(
+        'Message',
+        order_by='room.id',
+        back_populates='room',
+        lazy='joined',
+    )
+
+
+class Message(Base):
+    __tablename__ = 'message'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    sender_id = Column(
+        Integer,
+        ForeignKey('member.id'),
+    )
+    sender = relationship(
+        'Member',
+        back_populates='messages',
+        lazy='joined',
+    )
+    body = Column(
+        'body',
+        String,
+        nullable=True,
+    )
+    room_id = Column(
+        Integer,
+        ForeignKey('room.id'),
+    )
+    room = relationship(
+        'Room',
+        back_populates='messages',
+        lazy='joined',
+    )
+
+
+Base.metadata.create_all(bind=engine)
+
