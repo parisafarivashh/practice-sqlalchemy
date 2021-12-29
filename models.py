873298eaba9e2d21996dec5494980939a87abfd6
parisa <57132194+parisafarivashh@users.noreply.github.com>
2021-12-29 15:43:31 +0330

    Adding test for member, message and room models (#10)
    * Add file models.py
    
    * create member & room models
    
    * create message model
    
    * add primary_key to table members_room
    
    * add comment connection
    
    * change style coding
    
    * change style code table member_room
    
    * add file requirements.txt
    
    * change name of model members_room to member_room
    
    * Add a secondary in relation to the member model
    
    * delete comment connection because state the obvious
    
    * delete table member_room
    
    * Delete the empty space next to the table
    
    * add extra line to the end of the project
    
    * change name members relation to member in room model
    
    * change name member relation to creator in room model
    
    * change name member relation to sender in message model
    
    * add space after table name and after imports
    
    * change name relation between member and room models
    
    * change name relation between member and message models
    
    * change name back_populate relation room in message model
    
    * change double quotation to single quotation
    
    * install pytest and added in file requirements.txt
    
    * add setup for test
    
    * create instance member for test
    
    * create instance message and room
    
    * added test get query with id for room, message and member models
    
    * change code style Config
    
    * test update creator of room model
    
    * changed name test class to TestQuery
    
    * test update sender of message model
    
    * test update sender of message model
    
    * tested number of room that member created
    
    * test exist first title of member model
    
    * used cascade in relationship for delete
    
    * test order by member with id
    
    * changed code style test class
    
    * tested order by id of room model
    
    * tested order by id of message model
    
    * tested exist title of member with s
    
    * testes exist creator with p
    
    * tested exist sender with p in the message model
    
    * tested update first name of member model
    
    * changed code style update test
    
    * tested update title of room model
    
    * added function test update for all models
    
    * added function test with order by
    
    * changed code style of test class
    
    * added function test exists
    
    * change style code function exists
    
    * added function test limit in the query
    
    * tested quey one_or_none for all models
    
    * change code style function get member
    
    * tested exist none entity in member model
    
    * tested exist game room name or not
    
    * tested count message of member
    
    * change function name test filter to test count
    
    * add ondelete cascade on foreignkey
    
    * comment delete session
    
    * changed double cotation with single cotation
    
    * added title none in member model
    
    * changed title search for room model in test count
    
    * changed the class name testquery to test
    
    * added extra line the end of project
    
    * changed double cotation with single cotation
    
    * edit value entity of models
    
    * chnaged assert for test update
    
    * changed function test exist
    
    * changed code style function test exist
    
    * changed name variable of function test message
    
    * changed code style function test cout
    
    * changed style code test count_message
    
    * changed style code test count_creator_of_room
    
    * changed add_all to single add in function setup
    
    * changed filter_by to filter in test update
    
    * changed name of varibale edit to update in test update
    
    * changed the style of the code function test order_by
    
    * changed equal none to is true in test class

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
+import pytest
+
 from sqlalchemy import Column, ForeignKey, \
-    Integer, String, create_engine, Date
-from sqlalchemy.orm import relationship
+    Integer, String, create_engine, Date, func, or_, and_, exists
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
 engine = create_engine(
-    'postgresql+psycopg2://postgres:postgres@localhost/sqlalchemy_practice'
+    'postgresql+psycopg2://postgres:postgres@localhost/practice'
 )
 Base = declarative_base()
 
@@ -64,17 +66,19 @@ class Room(Base):
     creator_id = Column(
         'creator_id',
         Integer,
-        ForeignKey('member.id'),
+        ForeignKey(
+            'member.id',
+            ondelete='CASCADE',
+        ),
     )
     creator = relationship(
         'Member',
-        order_by='room.id',
+        cascade='all,delete',
         back_populates='creator_room',
         lazy='joined',
     )
     messages = relationship(
         'Message',
-        order_by='room.id',
         back_populates='room',
         lazy='joined',
     )
@@ -89,10 +93,14 @@ class Message(Base):
     )
     sender_id = Column(
         Integer,
-        ForeignKey('member.id'),
+        ForeignKey(
+            'member.id',
+            ondelete='CASCADE',
+        ),
     )
     sender = relationship(
         'Member',
+        cascade='all,delete',
         back_populates='messages',
         lazy='joined',
     )
@@ -103,14 +111,224 @@ class Message(Base):
     )
     room_id = Column(
         Integer,
-        ForeignKey('room.id'),
+        ForeignKey(
+            'room.id',
+            ondelete='CASCADE',
+        ),
     )
     room = relationship(
         'Room',
+        cascade='all,delete',
         back_populates='messages',
         lazy='joined',
     )
 
 
 Base.metadata.create_all(bind=engine)
+
+
+class Config:
+
+    @pytest.fixture
+    def setup(self):
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+        self.session.query(Member).delete()
+        self.session.query(Message).delete()
+        self.session.query(Room).delete()
+
+        self.member_1 = Member(
+            title='first_title',
+            first_name='first_name',
+            last_name='first_last_name'
+        )
+        self.session.add(self.member_1)
+
+        self.member_2 = Member(
+            title='second_title',
+            first_name='second_name',
+            last_name='second_last_name'
+        )
+        self.session.add(self.member_2)
+
+        self.room_1 = Room(
+            title='first_room',
+        )
+        self.session.add(self.room_1)
+
+        self.room_2 = Room(
+            title='second_room',
+        )
+        self.session.add(self.room_2)
+
+        self.message_1 = Message(
+            body='first_body',
+        )
+        self.session.add(self.message_1)
+
+        self.message_2 = Message(
+            body='second_body',
+        )
+        self.session.add(self.message_2)
+        self.session.commit()
+
+
+class Test(Config):
+
+    def test_update(self, setup):
+        update_first_name = self.session.query(Member) \
+            .filter(Member.id == self.member_1.id) \
+            .one()
+        update_first_name.first_name = 'update_first_name'
+        self.session.add(update_first_name)
+        self.session.commit()
+        assert update_first_name.id is not None
+
+        update_title_room = self.session.query(Room) \
+            .filter(Room.id == self.room_1.id) \
+            .one()
+        update_title_room.title = 'update_tittle'
+        self.session.add(update_title_room)
+        self.session.commit()
+        assert update_title_room.id is not None
+
+        update_body_message = self.session.query(Message) \
+            .filter(Message.id == self.message_1.id) \
+            .one()
+        update_body_message.body = 'update_body'
+        self.session.add(update_body_message)
+        self.session.commit()
+        assert update_body_message.id is not None
+
+    def test_order_by(self, setup):
+        member_order_by_id = self.session.query(Member) \
+            .order_by(Member.id) \
+            .all()
+        assert member_order_by_id[0].id <= member_order_by_id[1].id
+
+        room_order_by_id = self.session.query(Room) \
+            .order_by(Room.id) \
+            .all()
+        assert room_order_by_id[1].id >= room_order_by_id[0].id
+
+        message_order_by_id = self.session.query(Message) \
+            .order_by(Message.id) \
+            .all()
+        assert message_order_by_id[0].id <= message_order_by_id[1].id
+
+    def test_exists(self, setup):
+        exist_title_with_s = self.session.query(exists() \
+            .where(Member.title.startswith('s%'))) \
+            .scalar()
+        assert exist_title_with_s is True
+
+        exist_room_with_f = self.session.query(exists() \
+            .where(Room.title.startswith('f%'))) \
+            .scalar()
+        assert exist_room_with_f is True
+
+        exist_message_with_p = self.session.query(exists() \
+            .where(Message.body.startswith('s%'))) \
+            .scalar()
+        assert exist_message_with_p is True
+
+    def test_get_member(self, setup):
+        get_member = self.session.query(Member) \
+            .get(self.member_1.id)
+        assert get_member.first_name == 'first_name'
+
+        get_title = self.session.query(Member) \
+            .filter(Member.title == 'first_title') \
+            .first()
+        assert get_title.title == 'first_title'
+
+        check_id = self.session.query(Member) \
+            .filter(Member.id == 0) \
+            .one_or_none()
+        assert check_id is None
+
+    def test_get_room(self, setup):
+        room_1 = self.session.query(Room) \
+            .get(self.room_1.id)
+        assert room_1.title == 'first_room'
+
+        room_1.creator = self.member_1
+        self.session.add(room_1)
+        self.session.commit()
+        assert room_1.creator_id == self.member_1.id
+
+        check_id = self.session.query(Room) \
+            .filter(Room.id == 0) \
+            .one_or_none()
+        assert check_id is None
+
+    def test_get_message(self, setup):
+        message_1 = self.session.query(Message) \
+            .get(self.message_1.id)
+        assert message_1.body == 'first_body'
+
+        message_1.sender_id = self.member_2.id
+        self.session.add(message_1)
+        self.session.commit()
+        assert message_1.sender_id == self.member_2.id
+
+        check_id = self.session.query(Message) \
+            .filter(Message.id == 0) \
+            .one_or_none()
+        assert check_id is None
+
+    def test_limit(self, setup):
+        two_limit_member = self.session.query(Member) \
+            .limit(2) \
+            .all()
+        assert len(two_limit_member) == 2
+
+        two_limit_message = self.session.query(Message) \
+            .limit(2) \
+            .all()
+        assert len(two_limit_message) == 2
+
+        two_limit_room = self.session.query(Room) \
+            .limit(2) \
+            .all()
+        assert len(two_limit_room) == 2
+
+    def test_count(self, setup):
+        add_title_none = Member(
+            first_name='test_name',
+            last_name='test_last_name',
+        )
+        self.session.add(add_title_none)
+        self.session.commit()
+
+        exist_none_entity = self.session.query(Member.title) \
+            .filter(or_(
+                Member.title.is_(None),
+                Member.first_name.is_(None),
+                Member.last_name.is_(None)
+            )) \
+            .count()
+        assert exist_none_entity == 1
+
+        room_game = self.session.query(Room) \
+            .filter(Room.title.match('first_room')) \
+            .count()
+        assert room_game == 1
+
+        count_messages_of_member = self.session.query(func.count(Member.first_name)) \
+            .filter(and_(
+                Member.id == Message.sender_id,
+                Member.id == self.member_2.id
+            )) \
+            .scalar()
+        assert count_messages_of_member == 0
+
+        count_creator_of_room = self.session.query(func.count(Member.first_name)) \
+            .filter(and_(
+                Member.id == Room.creator_id,
+                Member.id == self.member_1.id
+            )) \
+            .scalar()
+        assert count_creator_of_room == 0
 
